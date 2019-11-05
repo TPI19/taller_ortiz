c33a9f87e90d2c809c6d6f581cf887e68bb068d3
unknown <peralta.gerardo97@gmail.com>
2019-11-04 20:30:39 -0600

    Se direccionaron los botones inicio, login, sing up, y el inicio, se hizo que login y sing up solo aparezcan en la pagina de inicio, anteriormente estaban en base y aparecian en todas las pantallas

--- taller_ortiz/settings.py
+++ taller_ortiz/settings.py
@@ -77,14 +77,12 @@ WSGI_APPLICATION = 'taller_ortiz.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.mysql',
-        'ENGINE': 'mysql_cymysql',
+        'ENGINE': 'django.db.backends.mysql',
         'NAME': 'taller',
         'USER': 'root',
-        'PASSWORD': 'root',
+        'PASSWORD': '',
         'HOST': 'localhost',
-        # 'PORT':3306,
-        'PORT':8889,
+        'PORT':3306,
         'OPTIONS': {
             'init_command': "SET sql_mode='STRICT_TRANS_TABLES'",
         },
