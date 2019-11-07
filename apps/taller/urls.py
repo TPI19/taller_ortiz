bc4315ef7f413a0b294779130388b4d74c48f339
renemarvera <renemarvera@gmail.com>
2019-11-06 19:48:20 -0600

    CRUD Vehiculos por Cliente: Listo sin validar formularios

--- apps/taller/urls.py
+++ apps/taller/urls.py
@@ -5,6 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('vehiculos/', views.vehiculos, name='vehiculos'),
+    path('agregar-vehiculo/', views.agregar_vehiculo, name='agregar_vehiculo'),
+    path('editar-vehiculo/', views.editar_vehiculo, name='editar_vehiculo'),
+    path('eliminar-vehiculo/', views.eliminar_vehiculo, name='eliminar_vehiculo'),
     path('detalle-vehiculo/<int:vehiculo_id>/', views.detalle_vehiculo, name='detalle_vehiculo'),
-    path('edicion-vehiculo/<int:vehiculo_id>/', views.edicion_vehiculo, name='edicion_vehiculo'),
 ]
--- apps/taller/views.py
+++ apps/taller/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse, Http404
 from .models import Vehiculo, Visita, Cliente
 
@@ -19,6 +19,39 @@ def vehiculos(request):
 	
 	return render(request, 'vehiculo/vehiculos.html', contexto)
 
+def agregar_vehiculo(request):
+
+	cliente = Cliente.objects.get(user = request.user.id)
+
+	vehiculo = Vehiculo()
+	vehiculo.cliente = cliente
+	vehiculo.placa = request.POST['placa']
+	vehiculo.tipo = request.POST['tipo']
+	vehiculo.marca = request.POST['marca']
+	vehiculo.modelo = request.POST['modelo']
+	vehiculo.anio = int(request.POST['anio'])
+	vehiculo.save()
+
+	return redirect('vehiculos')
+
+def editar_vehiculo(request):
+
+	vehiculo = Vehiculo.objects.get(pk=request.POST['id_edit'])
+	vehiculo.placa = request.POST['placa_edit']
+	vehiculo.tipo = request.POST['tipo_edit']
+	vehiculo.marca = request.POST['marca_edit']
+	vehiculo.modelo = request.POST['modelo_edit']
+	vehiculo.anio = int(request.POST['anio_edit'])
+	vehiculo.save()
+
+	return redirect('vehiculos')
+
+def eliminar_vehiculo(request):
+
+	Vehiculo.objects.filter(id=request.POST['id_delete']).delete()
+
+	return redirect('vehiculos')
+
 def detalle_vehiculo(request,vehiculo_id):
 
 	try:
@@ -30,21 +63,5 @@ def detalle_vehiculo(request,vehiculo_id):
 
 	return render(request, 'vehiculo/detalle.html', {'vehiculo': vehiculo})
 
-def edicion_vehiculo(request,vehiculo_id):
 
-	try:
-
-		vehiculo = Vehiculo.objects.get(pk=vehiculo_id)
-		vehiculo.placa = request.POST['placa']
-		vehiculo.tipo = request.POST['tipo']
-		vehiculo.marca = request.POST['marca']
-		vehiculo.modelo = request.POST['modelo']
-		vehiculo.anio = int(request.POST['anio'])
-		vehiculo.save()
-		# selected_choice = vehiculo.anio_set.get(pk=request.POST['choice'])
-
-	except Vehiculo.DoesNotExist:
-		return render(request, 'base/not_found.html')
-
-	return render(request, 'vehiculo/detalle.html', {'vehiculo': vehiculo})
 
